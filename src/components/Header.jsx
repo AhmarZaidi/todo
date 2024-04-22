f515c6048fb9d6a71cce737c92f25d5e92262b1b
AhmarZaidi <ahmarzaidi07@gmail.com>
2024-04-22 15:30:10 +0530

    Update footer text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,10 @@
-import React, { useState } from 'react'
-
-import ModeToggle from './ModeToggle'
+import React from 'react'
 
 const Header = () => {
 
-  const [darkMode, setDarkMode] = useState(true);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  }
-
   return (
     <header className='header'>
-      <div className='header-left'></div>
-
       <h2 className='site-title'>to-do</h2>
-
-      <div className='header-right'>
-        <ModeToggle darkMode={darkMode} onClick={toggleDarkMode}/>
-      </div>
     </header>
   )
 }
