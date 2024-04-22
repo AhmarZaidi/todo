dbf72f2175e1f86fe358b1a1f3dfcf7670bdae19
AhmarZaidi <ahmarzaidi07@gmail.com>
2024-04-22 15:13:39 +0530

    Add support for touch cursor

--- src/assets/scripts/cursor.js
+++ src/assets/scripts/cursor.js
@@ -1,16 +1,19 @@
 export function setupCursorMovement() {
-	document.body.onmousemove = function(e) {
-		document.documentElement.style.setProperty (
-		  '--x', (
-			e.clientX+window.scrollX
-		  )
-		  + 'px'
-		);
-		document.documentElement.style.setProperty (
-		  '--y', (
-			e.clientY+window.scrollY
-		  ) 
-		  + 'px'
-		);
-	}
+    function updatePosition(e) {
+        var x = e.clientX || e.touches[0].clientX;
+        var y = e.clientY || e.touches[0].clientY;
+
+        document.documentElement.style.setProperty('--x', (x + window.scrollX) + 'px');
+        document.documentElement.style.setProperty('--y', (y + window.scrollY) + 'px');
+    }
+
+    function hideCursor() {
+        document.documentElement.style.setProperty('--x', '-10000px');
+        document.documentElement.style.setProperty('--y', '-10000px');
+    }
+
+    document.body.onmousemove = updatePosition;
+    document.body.ontouchmove = updatePosition;
+    document.body.ontouchstart = updatePosition;
+    document.body.ontouchend = hideCursor;
 }
